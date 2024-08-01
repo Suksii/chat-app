69b38036a0aa4b774ed011425cb9fed4fb99e80d
unknown <ramovic225@gmail.com>
2024-08-01 14:32:51 +0200

    login and register page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,13 +7,13 @@ import Login from "./pages/Login.jsx";
 function App() {
 
   return (
-    <>
+    <div className="h-screen flex items-center justify-center">
       <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/register" element={<Register/>}/>
           <Route path="/login" element={<Login/>}/>
       </Routes>
-    </>
+    </div>
   )
 }
 

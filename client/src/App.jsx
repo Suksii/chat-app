cbaeb9550ac27c0d868a89e3d3685976130d09d1
unknown <ramovic225@gmail.com>
2024-08-01 13:29:23 +0200

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import {Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Register from "./pages/Register.jsx";
+import Login from "./pages/Login.jsx";
+
+function App() {
+
+  return (
+    <>
+      <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
+      </Routes>
+    </>
+  )
+}
+
+export default App

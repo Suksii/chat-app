d2a294c4ab64220a1dfa33c7aa0f7e752b1f566b
unknown <ramovic225@gmail.com>
2024-08-01 14:32:51 +0200

    login and register page added

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-           Login
-        </div>
-    );
+        return (
+            <div className="flex flex-col justify-center items-center min-w-96 mx-auto">
+                <div className="w-full bg-gray-100 bg-clip-padding backdrop-filter rounded-lg px-4 py-8 backdrop-blur-lg bg-opacity-10">
+                    <h1 className="text-3xl text-gray-300 text-center font-semibold">Login</h1>
+                    <form className="w-full flex flex-col items-center justify-center gap-2 py-4">
+                        <input placeholder="Type username" type="text" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                        <input placeholder="Type password" type="password" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                        <p className="text-gray-300 flex gap-2 items-center">
+                            <span>Don't have an account?</span>
+                            <Link to="/register" className="text-blue-600 font-semibold">Register here</Link>
+                        </p>
+                        <button className="btn glass btn-neutral text-xl w-full">Login</button>
+                    </form>
+                </div>
+            </div>
+        );
 };
 
 export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className="flex flex-col justify-center items-center min-w-96 mx-auto">
+            <div className="w-full bg-gray-100 bg-clip-padding backdrop-filter rounded-lg px-4 py-8 backdrop-blur-lg bg-opacity-10">
+                <h1 className="text-3xl text-gray-300 text-center font-semibold">Register</h1>
+                <form className="w-full flex flex-col items-center justify-center gap-2 py-4">
+                    <input placeholder="Type username" type="text" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                    <input placeholder="Type email" type="text" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                    <input placeholder="Type password" type="password" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                    <input placeholder="Comfirm password" type="password" className="input input-ghost w-full focus:bg-opacity-0 focus:outline-none" />
+                    <p className="text-gray-300 flex gap-2 items-center">
+                        <span>Already have an account?</span>
+                        <Link to="/login" className="text-blue-600 font-semibold">Login here</Link>
+                    </p>
+                    <button className="btn glass btn-neutral text-xl w-full">Register</button>
+                </form>
+            </div>
         </div>
     );
 };

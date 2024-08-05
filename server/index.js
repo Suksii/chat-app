0853ac8d4003c5b81677dab0a0e677763989fd22
unknown <ramovic225@gmail.com>
2024-08-05 17:55:09 +0200

    registration functionallity completed

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from "./routes/auth.js";
+import mongoose from 'mongoose'
+import cors from 'cors'
 
 const app = express();
 
@@ -8,10 +10,25 @@ dotenv.config()
 
 const PORT = process.env.PORT
 
-app.get("/",(req, res) => {
-    res.json("Hello World")
-})
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Connected to MongoDB")
+    } catch (error) {
+        console.error("Error connecting to MongoDB", error)
+    }
+}
+connectDB()
 
+// app.get("/",(req, res) => {
+//     res.json("Hello World")
+// })
+
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}));
 app.use("/api/auth", authRouter)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

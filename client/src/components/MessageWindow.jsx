72e854c565d784e6b61facfcce45b19bd043d6c9
unknown <ramovic225@gmail.com>
2024-08-05 14:28:48 +0200

    NoChatSelected component added

--- client/src/components/MessageWindow.jsx
+++ client/src/components/MessageWindow.jsx
@@ -2,17 +2,24 @@ import React from 'react';
 import UserAvatar from "./UserAvatar.jsx";
 import Messages from "./Messages.jsx";
 import MessageInput from "./MessageInput.jsx";
+import NoChatSelected from "./NoChatSelected.jsx";
 
 const MessageWindow = () => {
+
+    const isChatSelected = false;
+
     return (
-        <div style={{flex: 2}} className={"flex flex-col"}>
-            <div className="w-full flex gap-2 items-center bg-gray-100 bg-opacity-50 p-1">
-                <UserAvatar />
-                <p className="text-lg font-semibold">Full Name</p>
-            </div>
-                <Messages/>
-            <MessageInput/>
-        </div>
+        <>
+            {isChatSelected ? (
+            <div style={{flex: 2}} className={"flex flex-col"}>
+                <div className="w-full flex gap-2 items-center bg-gray-100 bg-opacity-50 p-1">
+                    <UserAvatar />
+                    <p className="text-lg font-semibold">Full Name</p>
+                </div>
+                    <Messages/>
+                <MessageInput/>
+            </div>) : <NoChatSelected/> }
+        </>
     );
 };
 

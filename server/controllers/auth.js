9f1309e4fd9c048bcd5dfa25baf4b2855e6d9730
unknown <ramovic225@gmail.com>
2024-08-05 18:23:56 +0200

    login functionallity completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,9 +1,10 @@
 import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
     try {
-        const { username, fullName, password, confirmPassword } = req.body;
+        const { username, fullName, password, confirmPassword, profilePicture } = req.body;
 
         if (password !== confirmPassword) {
             return res.status(400).json({ message: "Passwords do not match" })
@@ -18,7 +19,7 @@ export const register = async (req, res) => {
             username,
             fullName,
             password: bcrypt.hashSync(password, bycryptSalt),
-            // profilePicture
+            profilePicture
         })
             return res.status(201).send(newUser)
     } catch (error) {
@@ -26,8 +27,26 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.send("login")
+export const login = async (req, res) => {
+    const { username, password } = req.body;
+    try {
+        const user = await User.findOne({ username })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" })
+        }
+        const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+        jwt.sign({ username: user.username, id: user._id }, process.env.JWT_SECRET, {}, (err, token) => {
+            if (err) {
+                console.error(err)
+            }
+            res.cookie('token', token).json(user)
+        })
+    } catch (error) {
+        console.error(error)
+    }
 }
 
 export const logout = (req, res) => {

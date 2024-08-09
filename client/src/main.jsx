353e5a412ae146f97093966e7d2f89f0d2264ad6
unknown <ramovic225@gmail.com>
2024-08-09 23:41:29 +0200

    socketContext added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,13 +5,16 @@ import './index.css'
 import {BrowserRouter} from "react-router-dom";
 import {AuthProvider} from "./context/AuthContext.jsx";
 import {ConversationsProvider} from "./context/ConversationsContext.jsx";
+import {SocketProvider} from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
       <BrowserRouter>
           <AuthProvider>
               <ConversationsProvider>
-                  <App />
+                  <SocketProvider>
+                      <App />
+                  </SocketProvider>
               </ConversationsProvider>
           </AuthProvider>
       </BrowserRouter>

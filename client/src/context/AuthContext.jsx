6fb5bd92c7d83bc8ef6f4d258b7f375ff63710f7
unknown <ramovic225@gmail.com>
2024-08-19 14:08:16 +0200

    userProfile bug fixed

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -8,25 +8,25 @@ export const AuthProvider = ({children}) => {
     const [currentUser, setCurrentUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const getProfile = async () => {
-            setLoading(true)
-            try {
-                const response = await axios.get("/auth/profile");
-                setCurrentUser(response.data);
-                console.log(response.data)
-            } catch (error) {
-                console.error(error);
-            } finally {
-                setLoading(false);
-            }
+    const getProfile = async () => {
+        setLoading(true)
+        try {
+            const response = await axios.get("/auth/profile", {withCredentials: true});
+            setCurrentUser(response.data);
+        } catch (error) {
+            console.error(error);
+            setCurrentUser(null);
+        } finally {
+            setLoading(false);
         }
+    }
+
+    useEffect(() => {
         getProfile();
     }, []);
 
-
     return (
-        <AuthContext.Provider value={{currentUser, setCurrentUser, loading}}>
+        <AuthContext.Provider value={{currentUser, setCurrentUser, loading, getProfile}}>
             {children}
         </AuthContext.Provider>
     );

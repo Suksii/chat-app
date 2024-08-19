1215644c10a87188fe53b3b0b9e59803b0541e96
unknown <ramovic225@gmail.com>
2024-08-19 14:39:32 +0200

    login errors handled

--- client/src/hooks/useLogin.jsx
+++ client/src/hooks/useLogin.jsx
@@ -7,21 +7,24 @@ const useLogin = () => {
 
     const navigate = useNavigate();
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
     const {getProfile} = useAuth();
     const login = async (username, password) => {
         setLoading(true)
+        setError(null)
         try {
             await axios.post("/auth/login", {username, password});
             await getProfile();
             navigate("/");
         } catch (error) {
             console.error(error);
+            setError(error.response.data.message);
         } finally {
             setLoading(false);
         }
     }
 
-    return {login, loading};
+    return {login, loading, error};
 
 };
 

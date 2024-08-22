3ff24768f79ce8e60a5d698e1d20b19ef638f2f1
unknown <ramovic225@gmail.com>
2024-08-22 23:39:52 +0200

    is user online added

--- client/src/components/MessageWindow.jsx
+++ client/src/components/MessageWindow.jsx
@@ -4,9 +4,16 @@ import Messages from "./Messages.jsx";
 import MessageInput from "./MessageInput.jsx";
 import NoChatSelected from "./NoChatSelected.jsx";
 import {useConversations} from "../context/ConversationsContext.jsx";
+import {useSocket} from "../context/SocketContext.jsx";
 const MessageWindow = () => {
 
     const {selectedConversation, setSelectedConversation} = useConversations();
+
+    const lastSeen = selectedConversation?.lastSeen?.slice(11, 16);
+
+    const isOnline = selectedConversation?.online ? "Online" : `last seen ${lastSeen}`;
+
+    console.log(selectedConversation)
 
     useEffect(() => {
         return () => setSelectedConversation(null);
@@ -18,7 +25,10 @@ const MessageWindow = () => {
             <div style={{flex: 2}} className={"flex flex-col"}>
                 <div className="w-full flex gap-2 items-center bg-gray-100 bg-opacity-50 p-1">
                     <UserAvatar user={selectedConversation}/>
-                    <p className="text-lg font-semibold">{selectedConversation?.fullName}</p>
+                    <div className="flex items-end gap-2">
+                        <p className="text-lg font-semibold">{selectedConversation?.fullName}</p>
+                        <p className="text-sm text-gray-500">{isOnline}</p>
+                    </div>
                 </div>
                     <Messages/>
                 <MessageInput/>

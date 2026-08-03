2b7a1d92c8f9efbd3230572ce6f3e48b0939f1c0
Suco Ramovic <suco.ramovic@datadesign.me>
2026-08-03 19:27:56 +0200

    Keep the newMessage listener stable instead of rebuilding it on every message

--- client/src/hooks/useListenMessages.jsx
+++ client/src/hooks/useListenMessages.jsx
@@ -1,23 +1,22 @@
-import {useEffect} from 'react';
-import {useSocket} from "../context/SocketContext.jsx";
-import {useConversations} from "../context/ConversationsContext.jsx";
+import { useEffect } from "react";
+import { useSocket } from "../context/SocketContext.jsx";
+import { useConversations } from "../context/ConversationsContext.jsx";
 import notificationSound from "../assets/notification/notification.mp3";
 
 const UseListenMessages = () => {
+  const { socket } = useSocket();
+  const { setMessages } = useConversations();
 
-    const {socket} = useSocket();
-    const {messages, setMessages} = useConversations();
-
-    useEffect(() => {
-        socket?.on('newMessage', (message) => {
-            const notification = new Audio(notificationSound);
-            notification.play();
-            setMessages(messages => [...messages, message]);
-        });
-        return () => {
-            socket?.off('newMessage');
-        }
-    }, [socket, messages, setMessages]);
+  useEffect(() => {
+    const handleNewMessage = (message) => {
+      new Audio(notificationSound).play().catch(() => {});
+      setMessages((messages) => [...messages, message]);
+    };
+    socket?.on("newMessage", handleNewMessage);
+    return () => {
+      socket?.off("newMessage", handleNewMessage);
+    };
+  }, [socket, setMessages]);
 };
 
 export default UseListenMessages;
